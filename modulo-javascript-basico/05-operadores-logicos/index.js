976af3601e8519f9e7e5366c862c79ee9fdc2262
SrCooT <juno.lp@hotmail.com>
2023-09-06 23:49:18 -0300

    operadores logicos

---
+++ modulo-javascript-basico/05-operadores-logicos/index.js
@@ -0,0 +1,22 @@
+/*
+    == ---- igual ----- a==b ---- verdadeiro se for igual a b
+
+    === ----- identico ---- a===b ---- verdadeiro se a for identico a b
+
+    !== ----- não-identico ---- a !==b ---- verdadeiro se a nao for identico a b
+
+    != ----- diferente ----- a != b ---- verdadeiro se o a for diferente de b
+
+    <  ----- menor que  ----- a < b ---- verdadeiro quando a for menor que b
+
+    <= ---- menor igual que  ---- verdadeiro quando a for menor ou igual a b
+
+    > ---- maior i que ---- verdadeiro quando a for maior ou igual a b
+
+    >= ---- mairo igual que ---- a >= b ---- verdadeiro quando a for maior ou igual a b
+*/
+
+const a = 3
+const b = 3
+
+console.log(a >= b)
