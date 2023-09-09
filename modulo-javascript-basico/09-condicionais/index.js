3e593feb70c29588f34a737a0dc014b06801a500
SrCooT <juno.lp@hotmail.com>
2023-09-08 23:53:31 -0300

    considicionais

---
+++ modulo-javascript-basico/09-condicionais/index.js
@@ -0,0 +1,15 @@
+let nomeFilme = 'Vingadores'
+
+// if(nomeFilme === 'Vingadores'){
+//     console.log('É o filme do Vingadores')
+// }else if(nomeFilme === 'Batman vs Superman'){
+//     console.log('É o filme do Batman vs Superman')
+// }else if(nomeFilme === 'Capitão América'){
+//     console.log('É o filme do Capitão América')
+// }else{
+//     console.log('É outro filme')
+// }
+
+false ? console.lob('true') : console.log('false')
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos Vingadores') : console.log( 'é outro filme')
