a6d9b29be355aacbf80aa180c1aee0f9b45b1242
SrCooT <juno.lp@hotmail.com>
2023-09-11 20:41:32 -0300

    condicionais switch

---
+++ modulo-javascript-basico/10-condicionais-switch/index.js
@@ -0,0 +1,47 @@
+//switch(parametro){
+//  case valor1:
+//      <bloco de declaracao>
+//      break
+//  case valor2:
+//      <bloco de declaração>
+//      break
+//    defaut:
+//      console.log('declaracao')
+//      break
+// }
+
+//     let nomeFilme = 'Vingadores'
+
+// switch(nomeFilme){
+//     case 'Vingadores':
+//         console.log('É o filme dos vingadores')
+//         break
+//     case 'Batman vs Superman':
+//         console.log('É o filme do Batman vs Superman')
+//         break
+//     case 'Senhor dos Anéis':
+//         console.log('É o filme do Senhor dos Anéis')
+//     default:
+//         console.log('É outro filme')
+//         break
+// }
+
+
+let avaliacao = 4
+
+switch(avaliacao){
+    case 1:
+    case 2:
+        console.log('Filme Ruim')
+        break
+    case 3:
+    case 4:
+        console.log('Filme Mediano')
+        break
+    case 5:
+        console.log('File excelente')
+        break
+    default:
+        console.log('Nota invalida')
+        break
+}
