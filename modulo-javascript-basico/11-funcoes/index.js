369a960ea12859a864dc742c92bff0dd52b906c6
SrCooT <juno.lp@hotmail.com>
2023-09-11 21:11:55 -0300

    funcoes

---
+++ modulo-javascript-basico/11-funcoes/index.js
@@ -0,0 +1,33 @@
+/*
+    function nomeDaFuncao(){
+        <bloco de execução>
+    }
+
+    nomeDaFuncao()
+*/
+
+/*
+    function nomeDaFuncao(parametro1, parametro2){
+        <bloco de execução>
+    }
+
+    nomeDaFuncao(parametro1, parametro2)
+*/
+
+function incentivarQuester(nomeQuester = 'Quester')
+{
+    alert('Muito bem ' + nomeQuester +  ', voce chegou no JS, continue assim!')
+
+}
+
+incentivarQuester('Juno')
+
+// function soma(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = soma(1, 3)
+// console.log(resultadoDaSoma)//4
+
+// resultadoDaSoma = soma(resultadoDaSoma, 10)
+// console.log(resultadoDaSoma)//14
