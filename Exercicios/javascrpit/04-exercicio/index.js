ce78ec69ef52bacfda726a35479fdb1487a187e7
SrCooT <juno.lp@hotmail.com>
2023-09-13 23:03:51 -0300

    novo modo de resolucao adicionado

--- Exercicios/javascrpit/04-exercicio/index.js
+++ Exercicios/javascrpit/04-exercicio/index.js
@@ -4,8 +4,9 @@ do{
     console.log('o numero atual é o: ' + i)
     i++
 }while(i <=10)
+
 /* Resolvendo com o for
 for (let i = 0; i <= 10; i++) {
     console.log('o numero atual é o :' + i);
     
-}
+}*/
