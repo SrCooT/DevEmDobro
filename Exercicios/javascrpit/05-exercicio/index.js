702780b0a634a94c2eeaf926fea2bc0d29e75764
SrCooT <juno.lp@hotmail.com>
2023-09-14 00:01:28 -0300

    mais um exemplo adicionado de resolução

--- Exercicios/javascrpit/05-exercicio/index.js
+++ Exercicios/javascrpit/05-exercicio/index.js
@@ -1,32 +1,15 @@
-for(let i =0; i <= 10; i++){
-   console.log(' o dobro do numero ' + i + ' é ' + i * 2)
-}
+// for(let i =0; i <= 10; i++){
+//    console.log(' o dobro do numero ' + i + ' é ' + i * 2)
+// }
 
-for(let i =0; i <= 10; i++){
-   console.log(' o triplo do numero ' + i + ' é ' + i * 3)
-}
-for(let i =0; i <= 10; i++){
-   console.log(' o quadruplo do numero ' + i + ' é ' + i * 4)
-}
-for(let i =0; i <= 10; i++){
-   console.log(' o quintuplo do numero ' + i + ' é ' + i * 5)
-}
-for(let i =0; i <= 10; i++){
-   console.log(' o sexto do numero ' + i + ' é ' + i * 6)
-}
+// for(let i =0; i <= 10; i++){
+//    console.log(' o triplo do numero ' + i + ' é ' + i * 3)
+// }
 
-for(let i =0; i <= 10; i++){
-   console.log(' o setimo do numero ' + i + ' é ' + i * 7)
-}
 
-for(let i =0; i <= 10; i++){
-   console.log(' o oitavo do numero ' + i + ' é ' + i * 8)
-}
+let i = 0
 
-for(let i =0; i <= 10; i++){
-   console.log(' o nono do numero ' + i + ' é ' + i * 9)
-}
-
-for(let i =0; i <= 10; i++){
-   console.log(' o descimo do numero ' + i + ' é ' + i * 10)
-}
+do{
+   console.log('o dobro do numero ' + i + ' é ' + i * 2 )
+   i++
+}while(i <= 10)
