15273bd973c5136078b9f549fb5d5a6aa7dcc7fe
SrCooT <juno.lp@hotmail.com>
2023-09-17 20:27:10 -0300

    arrow avante

---
+++ modulo-javascript-intermediario/04-arrow-function/index.js
@@ -0,0 +1,13 @@
+// let resultadoDaSoma = function(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// console.log(resultadoDaSoma(12, 5))
+
+nomeQuester = prompt('Qual o seu nome')
+
+const incentivarQuester = nomeQuester => {
+    console.log('Parabéns ' + nomeQuester + ' , Você chegou ao módulo de JS intermediario')
+}
+
+incentivarQuester(nomeQuester)
