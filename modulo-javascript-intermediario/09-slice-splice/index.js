010da6742cc25da3964d3ea65a99e8d0d81edd79
SrCooT <juno.lp@hotmail.com>
2023-09-17 23:06:38 -0300

    slice e splice aprendido

---
+++ modulo-javascript-intermediario/09-slice-splice/index.js
@@ -0,0 +1,20 @@
+let frutas = ['Banana', 'Maça', 'Laranja', 'Uva']
+
+// let frutasExtraidas = frutas.slice(1, 3)
+
+// console.log(frutasExtraidas)
+
+// console.log(frutas)
+
+// let frutasExtraidas = frutas.splice(1, 2)
+
+// console.log(frutasExtraidas)
+
+
+// console.log(frutas)
+
+
+let removeEAdicionaFrutas = frutas.splice(1 , 2, 'Goiaba', 'Abacaxi', 'jaboticaba')
+
+console.log(frutas)
+console.log(removeEAdicionaFrutas)
