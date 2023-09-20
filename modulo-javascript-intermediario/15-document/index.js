d28bbd03172ac81c7816b94a3fa993be723f3d8c
SrCooT <juno.lp@hotmail.com>
2023-09-19 22:38:56 -0300

    aprensendo sobre DOM

---
+++ modulo-javascript-intermediario/15-document/index.js
@@ -0,0 +1,20 @@
+console.log(document)
+
+let p = document.getElementById('paragrafo1');
+
+console.log(p)
+
+
+let inputsPorClasse = document.getElementsByClassName('tag-de-input');
+
+console.log(inputsPorClasse)
+
+
+let inputsPorTags = document.getElementsByTagName('h1');
+
+console.log(inputsPorTags);
+
+
+let  inputPorName = document.querySelectorAll("input[name ='email']");
+
+console.log(inputPorName)
