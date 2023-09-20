ea9a02322bc0f7806d0d93dbd489e2a45fb07ba8
SrCooT <juno.lp@hotmail.com>
2023-09-19 21:24:04 -0300

    javascript a todo o vapor

---
+++ modulo-javascript-intermediario/13-objetos-arrays/index.js
@@ -0,0 +1,17 @@
+let frutas = ['uva', 'banana', 'mamao']
+
+let videoGame = {
+    name: 'Xbox',
+    valor: 3000
+}
+
+let videoGame2 = {
+    name: 'playstation',
+    valor: 5000
+}
+
+let videoGames = [videoGame, videoGame2]
+
+console.log(videoGames)
+
+console.log(videoGames[1].name)
