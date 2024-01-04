6dada365b1f7de4344848b8f4dc3e329f89ee2f0
Juno Crisitan <juno.lp@hotmail.com>
2024-01-03 22:40:10 -0300

    finalizado

--- projeto-fetch-github-api/src/scripts/index.js
+++ projeto-fetch-github-api/src/scripts/index.js
@@ -7,8 +7,10 @@ import { screen } from '/src/scripts/objects/screen.js';
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
+    if (validateEmptyInput(userName)) return
     getUserData(userName)
 })
+
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
     const userName = e.target.value
@@ -16,15 +18,28 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const isEnterKeyPressed = key === 13
 
     if (isEnterKeyPressed) {
+        if (validateEmptyInput(userName)) return
         getUserData(userName)
     }
 })
 
-
+function validateEmptyInput(userName) {
+    if (userName.length === 0) {
+        alert('Preencha o campo com o nome do usuario do GitHub')
+        return true
+    }
+}
 
 async function getUserData(userName) {
 
     const userResponse = await getUser(userName)
+
+    if(userResponse.message === "Not Found"){
+        screen.renderNotFound()
+        return
+    }
+
+
     const repositoriesResponse = await getRepositories(userName)
 
 
@@ -32,6 +47,6 @@ async function getUserData(userName) {
     user.setRepositories(repositoriesResponse)
     console.log(user);
 
-    screen.renderUser(user)   
+    screen.renderUser(user)
 }
 
