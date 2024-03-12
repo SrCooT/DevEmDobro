86311f2af1b78c5853aeb1893301f6c83250c3ac
Juno Crisitan <juno.lp@hotmail.com>
2024-03-11 22:14:45 -0300

    hooks custom + logica

---
+++ modulo-react-avancado/react-custom-hook/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import SaveButton from './components/saveButton/Savebutton'
+import StatusBar from './components/statusBar/StatusBar'
+
+function App() {
+
+  return (
+    <>
+
+    <StatusBar />
+    
+    <SaveButton />
+    
+    </>
+  )
+}
+
+export default App
---
+++ modulo-react-avancado/react-custom-hook/src/components/saveButton/Savebutton.jsx
@@ -0,0 +1,20 @@
+import { useOnlineStatus } from "../../assets/hooks/useOnlineStatus";
+
+function SaveButton() {
+    const isOnline = useOnlineStatus();
+
+
+    function handleSaveClick() {
+        console.log("Save Clicked");
+    }
+
+    return (
+        <>
+        <button disabled={!isOnline} onClick={handleSaveClick}>
+            {isOnline ? "Save progress" : "Reconecting..."}
+        </button>
+        </>
+    );
+}
+
+export default SaveButton;
